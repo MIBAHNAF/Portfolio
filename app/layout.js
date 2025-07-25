928c0422ed499723aa0c4cd333d290215cd85e41
MIBAHNAF <mirahnaf.ali001@umb.edu>
2025-07-25 00:05:55 -0400

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { Analytics } from "@vercel/analytics/next"
 
 const outfit =Outfit({
   subsets: ["latin"], weight: ["400", "500", "600", "700"],
@@ -32,6 +33,7 @@ export default function RootLayout({ children }) {
             {children}
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
