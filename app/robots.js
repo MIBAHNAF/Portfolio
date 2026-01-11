817343d8b847d98bbe98f14c674125aaccee6556
MIBAHNAF <mirahnaf.ali001@umb.edu>
2026-01-11 17:12:23 -0500

    indexing solved

--- app/robots.js
+++ app/robots.js
@@ -1,10 +1,12 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/user-image.png', '/user-image-*.png'],
+        disallow: ['/private/', '/footer-bg-color.png'],
+      }
+    ],
     sitemap: 'https://mirahnaf-ali.com/sitemap.xml',
   }
 }
